8e157036b3ed406e34a6e3fa4f3437214a293737
Rosen <Rosen9212@gmail.com>
2016-10-04 14:25:23 +0800

    label data from 1

--- labeling.py
+++ labeling.py
@@ -1,13 +1,14 @@
-EMOTION_DATASET = 'data/EmotionSongs/Dataset'
-feature_types = ['mvd', 'rh', 'rp', 'ssd', 'trh', 'tssd', 'mfcc']
+# DATASET = 'data/EmotionSongs/Dataset'
+DATASET = 'data/GTZAN/Dataset'
+feature_types = ['mvd', 'rh', 'rp', 'ssd', 'trh', 'tssd']
 
-names_file = open(EMOTION_DATASET + '/mp3names.csv', 'r')
-label_file = open(EMOTION_DATASET + '/labels.csv', 'w')
+names_file = open(DATASET + '/mp3names.csv', 'r')
+label_file = open(DATASET + '/labels.csv', 'w')
 
 count = 0
 for name in names_file: 
     name = name.split('####')
     name = [ item.strip() for item in name]
-    count += 1
     print count
     label_file.write('%s\t####\t%d\t####\t%s\n' % (name[0], count, name[1]))
+    count += 1
