803bc9b7066fffdcc05b4bfb52cad7402ed102b0
Rosen <Rosen9212@gmail.com>
2016-10-04 14:28:39 +0800

    change name

--- batchExtraction.sh
+++
@@ -1,3 +0,0 @@
-python rp_extract/rp_extract_batch.py -a data/EmotionSongs/Relax_all data/EmotionSongs/Features/Relax
-python rp_extract/rp_extract_batch.py -a data/EmotionSongs/Sad_all data/EmotionSongs/Features/Sad
-python rp_extract/rp_extract_batch.py -a data/EmotionSongs/Happy_all data/EmotionSongs/Features/Happy
